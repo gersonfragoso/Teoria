fae8d275b9862722b4b800c68f98a390dc577aa1
caiowgvo <caiowallacegu@hotmail.com>
2021-07-12 13:00:07 -0300

    Bug na Referencias corrigido - Trabalho Concluido

--- TesteWebScrappes/ArquivoCaio.py
+++ TesteWebScrappes/ArquivoCaio.py
@@ -21,47 +21,57 @@ def testeLink(link): #Testando se o link é da wikipedia
         else:
                 print(">>       Link Invalido        <<")
                 return ehValido == False
-       
-requestOk = False
-ehWiki = False
-while True:
-        artigoDesejado = str(input('    Digite sua url: ')) #pegando url
-        ehWiki = testeLink(artigoDesejado)
-        if not ehWiki:
-                requestOk = testeSite(artigoDesejado)
-        if not requestOk and not ehWiki:
-                break     
-        else:
-                print('Verifique o link e tente novamente')
 
-qualSeuDesejo = int(input('     Você deseja buscar por: \n'
-+'       1 - Indices \n'
-+'       2 - Referencias \n'
-+'       3 - Imagens \n'
-+'      Escolha sua opção:'))
+def main():       
+        requestOk = False
+        ehWiki = False
+        while True:
+                artigoDesejado = str(input('    Digite sua url: ')) #pegando url
+                ehWiki = testeLink(artigoDesejado)
+                if not ehWiki:
+                        requestOk = testeSite(artigoDesejado)
+                if not requestOk and not ehWiki:
+                        break     
+                else:
+                        print('Verifique o link e tente novamente')
 
-html = requests.get(artigoDesejado)
-html_content = html.content
-soup = BeautifulSoup(html_content, "html.parser")
+        qualSeuDesejo = int(input('     Você deseja buscar por: \n'
+        +'       1 - Indices \n'
+        +'       2 - Referencias \n'
+        +'       3 - Imagens \n'
+        +'      Escolha sua opção:'))
 
-if qualSeuDesejo == 1 :
-        indiceGeral = soup.find_all('span',{'class':'toctext'})
-        indicesRegex = re.compile(r'\>\w*\<' ) 
-        indices = re.findall(indicesRegex , str(indiceGeral))
-        indicesSemSimbolos = re.sub('[<>]', '', str(indices))
-        print(indicesSemSimbolos)
+        html = requests.get(artigoDesejado)
+        html_content = html.content
+        soup = BeautifulSoup(html_content, "html.parser")
 
-if qualSeuDesejo == 2 :
-       artigo = soup.find_all('a',  {"class":"mw-redirect"}) 
-       linkRegex = re.compile(r'\/wiki\/\S*' ) 
-       link = re.findall(linkRegex , str(artigo))
-       linksemAspas = re.sub('["]', '', str(link))
-       linkSemRepeticao =  list(set(linksemAspas))
-       print(linkSemRepeticao)
-             
-if qualSeuDesejo == 3 :
-       artigo = soup.find_all('div',  {"id": "bodyContent"}) 
-       imgRegex = re.compile(r'\w+\.jpg|\:\w+\.png|\w+\.svg' ) 
-       imagem = re.findall(imgRegex , str(artigo))
-       imagensSemRepeticao =  list(set(imagem))
-       print(imagensSemRepeticao)
+        if qualSeuDesejo == 1 :
+                indiceGeral = soup.find_all('span',{'class':'toctext'})
+                indicesRegex = re.compile(r'\>\w*\<' ) 
+                indices = re.findall(indicesRegex , str(indiceGeral))
+                indicesSemSimbolos = re.sub('[<>]', '', str(indices))
+                print(indicesSemSimbolos)
+
+        if qualSeuDesejo == 2 :
+                artigo = soup.find_all('a',  {"class":"mw-redirect"}) 
+                linkRegex = re.compile(r'\/wiki\/\w*' ) 
+                link = re.findall(linkRegex , str(artigo))
+                linksemAspas = re.sub('["]', '', str(link))
+                linkSemRepeticao =  list(set(linksemAspas))
+                print(linksemAspas)
+                
+        if qualSeuDesejo == 3 :
+                artigo = soup.find_all('div',  {"id": "bodyContent"}) 
+                imgRegex = re.compile(r'\w+\.jpg|\:\w+\.png|\w+\.svg' ) 
+                imagem = re.findall(imgRegex , str(artigo))
+                imagensSemRepeticao =  list(set(imagem))
+                print(imagensSemRepeticao)
+
+main()
+qualSeuDesejo = str(input('     Deseja fazer outra busca? S/N \n'))
+
+if qualSeuDesejo == "S" :
+        main()   
+          
+if qualSeuDesejo == 'N' :
+        main()
